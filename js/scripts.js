50ebb070331d260a054e74c5b81f2a7f7b086d94
ZEvans1 <zte.zachary@gmail.com>
2017-12-15 14:36:01 -0800

    Add a side bar for selected toppings,a reset button, and edited page text HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@
 
 //ingredient definitions
 var selectedToppings = [];
-var peppers = { name: "Peppers", price: 0.50 };
+var peppers = { name: "Peppers", price: 0.55 };
 var redOnions = { name: "Red onions", price: 0.25 };
 var mushrooms = { name: "Mushrooms", price: 0.75 };
-var extraCheese = { name: "Extra cheese", price: 0.25 };
-var parmesan = { name: "Parmesan", price: 0.50 };
-var mozzarella = { name: "Mozzarella", price: 0.50 };
+var cheese = { name: "Cheese", price: 0.25 };
+var parmesan = { name: "Parmesan", price: 0.55 };
+var mozzarella = { name: "Mozzarella", price: 0.55 };
 var tomatoSauce = { name: "Tomaoto sauce", price: 0.25 };
 var bbqSauce = { name: "BBQ sauce", price: 0.75 };
-var pestoSauce = { name: "Pesto sauce", price: 1.00 };
+var pestoSauce = { name: "Pesto sauce", price: 1.05 };
 var garlicSauce = { name: "Garlic sauce", price: 0.75 };
 
 var small = { name: "Small", price: 4.99 };
@@ -25,14 +25,14 @@ function Pizza(size, toppings) {
 }
 
 //determines price of order
-Pizza.prototype.price = function() {
+Pizza.prototype.calculateCost = function() {
   var sizePrice = this.pizzaSize.price;
   var toppingsPrice = 0;
 
   for (i=0; i<=this.pizzaToppings.length-1; i++) {
     toppingsPrice = toppingsPrice + this.pizzaToppings[i].price;
   }
-  this.pizzaPrice = sizePrice + toppingsPrice;
+  this.pizzaPrice = (sizePrice + toppingsPrice).toFixed(2);
 }
 
 //front end------------------------------------------------
@@ -40,15 +40,22 @@ $(document).ready(function() {
   $("#topping-button").click(function(event) {
     event.preventDefault();
     var selectedTopping = eval($("#toppings").val());
+    $("#topping-list").append("<li class='empty'>" + selectedTopping.name + ": $" + selectedTopping.price + "</li>");
     selectedToppings.push(selectedTopping);
-    console.log(selectedToppings);
+    console.log(selectedTopping);
   });
   $("form#new-order").submit(function(event) {
     event.preventDefault();
     var selectedSize = eval($("#sizes").val());
     var newPizza = new Pizza(selectedSize, selectedToppings);
     console.log(newPizza);
-    newPizza.price();
+    newPizza.calculateCost();
+    $("#hide").show();
     $("#pizza-cost").text("$" + newPizza.pizzaPrice);
+  });
+  $("#another-pizza").click(function(event) {
+    event.preventDefault();
+    $(".empty").remove();
+    $("#hide").hide();
   });
 });
