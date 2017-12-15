162f2e94afea35fc853b7e4829db63c14b6660bf
ZEvans1 <zte.zachary@gmail.com>
2017-12-15 10:53:28 -0800

    Define topping variables, create pizza constructor, allow topping objects to be placed in pizza constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 //back end
-var toppings = [];
+var selectedToppings = [];
+var peppers = { name: "Peppers", price: 0.50 };
+var redOnions = { name: "Red onions", price: 0.25 };
+var mushrooms = { name: "Mushrooms", price: 0.75 };
+var extraCheese = { name: "Extra cheese", price: 0.25 };
+var parmesan = { name: "Parmesan", price: 0.50 };
+var mozzarella = { name: "Mozzarella", price: 0.50 };
+var tomatoSauce = { name: "Tomaoto sauce", price: 0.25 };
+var bbqSauce = { name: "BBQ sauce", price: 0.75 };
+var pestoSauce = { name: "Pesto sauce", price: 1.00 };
+var garlicSauce = { name: "Garlic sauce", price: 0.75 };
+
+var small = { name: "Small", price: 4.00 };
+var medium = { name: "Medium", price: 7.00 };
+var large = { name: "Large", price: 9.00 };
 
 function Pizza(size, toppings) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
 }
 
+Pizza.prototype.price = function() {
+
+}
 
 //front end
 $(document).ready(function() {
   $("#topping-button").click(function(event) {
     event.preventDefault();
-    var selectedTopping = $("#toppings").val();
-    toppings.push(selectedTopping);
-    console.log(toppings);
+    var selectedTopping = eval($("#toppings").val());
+    selectedToppings.push(selectedTopping);
+    console.log(selectedToppings);
   });
   $("form#new-order").submit(function(event) {
     event.preventDefault();
-    var selectedSize = $("#sizes").val();
+    var selectedSize = eval($("#sizes").val());
+    var newPizza = new Pizza(selectedSize, selectedToppings);
+    console.log(newPizza);
   });
 });
